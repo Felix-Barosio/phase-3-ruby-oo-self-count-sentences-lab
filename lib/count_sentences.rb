3d707936f3d22f0d4341cc7b754481b9a1993dfa
Felix <barosiofelix@gmail.com>
2022-10-20 08:21:00 +0300

    Completed Assiignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
+    # binding.pry
   end
 
   def question?
-
+    self.end_with?('?')
+    # binding.pry
   end
 
   def exclamation?
-
+    self.end_with?('!')
+    # binding.pry
   end
 
   def count_sentences
-
+    arr = self.split(/[.?!]/)
+    arr.each do |line|
+      if line == ''
+        arr.delete(line)
+      end
+    end
+    arr.length
   end
 end
